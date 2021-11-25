236eaa7dfc74ca73cd68c43fc53d40695067feb2
powercrystals <3174294+powercrystals@users.noreply.github.com>
2021-11-25 10:02:06 -0500

    Apply ore sorting multiplier to higher-tier sorting recipes (ones requiring sorter 2+s such as most/all of Clown's)

--- mod-support/angel/fixes/sorting-multiplier.lua
+++ mod-support/angel/fixes/sorting-multiplier.lua
@@ -1,6 +1,6 @@
 -- ore sorting output scaling
 for name, recipe in pairs(data.raw["recipe"]) do
-	if recipe.category == "ore-sorting" then
+	if recipe.category == "ore-sorting" or recipe.category == "ore-sorting-2" or recipe.category == "ore-sorting-3" or recipe.category == "ore-sorting-4" then
 		if recipe.results then
 			for _, result in pairs(recipe.results) do
 				result.amount = result.amount * settings.startup["guft-balance-angel-ore-sort-multiplier"].value
