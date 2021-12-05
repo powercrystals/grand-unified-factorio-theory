6684e56dc9aea99d550efd4eb260b638c7bb22b9
powercrystals <3174294+powercrystals@users.noreply.github.com>
2021-12-04 21:36:14 -0500

    Expand bio module pollution reversal to levels 4-8

--- mod-support/angel/fixes/module-fixes.lua
+++ mod-support/angel/fixes/module-fixes.lua
@@ -3,4 +3,9 @@ if data.raw.module["angels-bio-yield-module"] and data.raw.module["angels-bio-yi
 	data.raw.module["angels-bio-yield-module"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module"].effect["pollution"].bonus
 	data.raw.module["angels-bio-yield-module-2"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-2"].effect["pollution"].bonus
 	data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	data.raw.module["angels-bio-yield-module-4"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	data.raw.module["angels-bio-yield-module-5"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	data.raw.module["angels-bio-yield-module-6"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	data.raw.module["angels-bio-yield-module-7"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	data.raw.module["angels-bio-yield-module-8"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
 end
