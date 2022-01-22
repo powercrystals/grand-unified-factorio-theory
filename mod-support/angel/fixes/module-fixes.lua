ef162c8b5f65a58384c64b7bae5f76ba14a22f41
powercrystals <3174294+powercrystals@users.noreply.github.com>
2022-01-22 08:55:44 -0500

    Verify Angel's Bio modules > 3 exist before trying to modify them

--- mod-support/angel/fixes/module-fixes.lua
+++ mod-support/angel/fixes/module-fixes.lua
@@ -3,9 +3,25 @@ if data.raw.module["angels-bio-yield-module"] and data.raw.module["angels-bio-yi
 	data.raw.module["angels-bio-yield-module"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module"].effect["pollution"].bonus
 	data.raw.module["angels-bio-yield-module-2"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-2"].effect["pollution"].bonus
 	data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
-	data.raw.module["angels-bio-yield-module-4"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
-	data.raw.module["angels-bio-yield-module-5"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
-	data.raw.module["angels-bio-yield-module-6"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
-	data.raw.module["angels-bio-yield-module-7"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
-	data.raw.module["angels-bio-yield-module-8"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+
+	-- angel's doesn't create modules 4+ if Bob's modules isn't present
+	if data.raw.module["angels-bio-yield-module-4"] then
+		data.raw.module["angels-bio-yield-module-4"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	end
+
+	if data.raw.module["angels-bio-yield-module-5"] then
+		data.raw.module["angels-bio-yield-module-5"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	end
+
+	if data.raw.module["angels-bio-yield-module-6"] then
+		data.raw.module["angels-bio-yield-module-6"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	end
+
+	if data.raw.module["angels-bio-yield-module-7"] then
+		data.raw.module["angels-bio-yield-module-7"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	end
+
+	if data.raw.module["angels-bio-yield-module-8"] then
+		data.raw.module["angels-bio-yield-module-8"].effect["pollution"].bonus = -data.raw.module["angels-bio-yield-module-3"].effect["pollution"].bonus
+	end
 end
